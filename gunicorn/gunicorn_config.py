c26948264d0cf7a5bdc4e6e721db64f3a783450b
yangtiger2005 <yangtiger2005@163.com>
2021-10-14 13:10:33 +0800

    在athena中，写一个路由函数，并配置路由

--- gunicorn/gunicorn_config.py
+++ gunicorn/gunicorn_config.py
@@ -4,16 +4,17 @@ import logging.handlers
 from logging.handlers import WatchedFileHandler
 import os
 import multiprocessing
-bind = '0.0.0.0:8000'      #绑定ip和端口号
-backlog = 512                #监听队列
-chdir = '/opt/workplace/hellodjango'  #gunicorn要切换到的目的工作目录
-timeout = 30      #超时
-worker_class = 'gevent' #使用gevent模式，还可以使用sync 模式，默认的是sync模式
-pidfile = '/opt/workplace/gunicorn/gunicorn.pid' #保存gunicorn的进程pid的文件
-workers = multiprocessing.cpu_count() * 2 + 1    #进程数
-threads = 2 #指定每个进程开启的线程数
-loglevel = 'debug' #日志级别，这个日志级别指的是错误日志的级别，而访问日志的级别无法设置
-access_log_format = '%(t)s %(p)s %(h)s "%(r)s" %(s)s %(L)s %(b)s %(f)s" "%(a)s"'    #设置gunicorn访问日志格式，错误日志无法设置
+
+bind = '0.0.0.0:8000'  # 绑定ip和端口号
+backlog = 512  # 监听队列
+chdir = '/opt/workplace/hellodjango'  # gunicorn要切换到的目的工作目录
+timeout = 30  # 超时
+worker_class = 'gevent'  # 使用gevent模式，还可以使用sync 模式，默认的是sync模式
+pidfile = '/opt/workplace/gunicorn/gunicorn.pid'  # 保存gunicorn的进程pid的文件
+workers = multiprocessing.cpu_count() * 2 + 1  # 进程数
+threads = 2  # 指定每个进程开启的线程数
+loglevel = 'debug'  # 日志级别，这个日志级别指的是错误日志的级别，而访问日志的级别无法设置
+access_log_format = '%(t)s %(p)s %(h)s "%(r)s" %(s)s %(L)s %(b)s %(f)s" "%(a)s"'  # 设置gunicorn访问日志格式，错误日志无法设置
 
 """
 其每个选项的含义如下：
@@ -31,5 +32,5 @@ D          request time in microseconds
 L          request time in decimal seconds
 p          process ID
 """
-accesslog = "/opt/workplace/gunicorn/logs/access.log"      #访问日志文件
-errorlog = "/opt/workplace/gunicorn/logs/error.log"        #错误日志文件
+accesslog = "/opt/workplace/gunicorn/logs/access.log"  # 访问日志文件
+errorlog = "/opt/workplace/gunicorn/logs/error.log"  # 错误日志文件
