69908e135e1d505893ebf0f7170e0011f2320ed8
yangtiger2005 <yangtiger2005@163.com>
2021-10-14 19:27:45 +0800

    定义模型

--- Athena/models.py
+++ Athena/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.IntegerField(default=24)
+    sex = models.CharField(max_length=1, default='0')
+    address = models.CharField(max_length=100, null=True)
