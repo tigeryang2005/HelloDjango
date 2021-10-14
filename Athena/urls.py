3f63b4ec13cfc903dff4d24db2ee8f87b2acd84f
yangtiger2005 <yangtiger2005@163.com>
2021-10-14 18:44:40 +0800

    修改正则

--- Athena/urls.py
+++ Athena/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
      path('', views.hello_world),
-     path('index', views.index),
+     re_path('^index/$', views.index),
      re_path(r'^index/(?P<year>[0-9]{4})/$', views.index_year),
      re_path(r'^index/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', views.index_year_month, name='index_year_month'),
 ]
