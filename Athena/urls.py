832e6d574421a7ea084b03db80acd1ea06f021e9
yangtiger2005 <yangtiger2005@163.com>
2021-10-14 13:52:21 +0800

    在hellodjango/目录下新建template文件夹和athena_templates，在athena_templates文件夹中创建html文件，在视图函数中使用模板

--- Athena/urls.py
+++ Athena/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.hello_world),
+    path('index', views.index),
 ]
--- Athena/views.py
+++ Athena/views.py
@@ -3,5 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+# 字符串返回
+def hello_world(request):
+    return HttpResponse('hello world!')
+
+
+# 模板返回
 def index(request):
-    return HttpResponse('hello world')
+    return render(request, 'athena_templates/index.html')
