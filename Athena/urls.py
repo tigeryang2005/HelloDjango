037ef07d4d2d11c3685fa6845cb73e257db7973b
yangtiger2005 <yangtiger2005@163.com>
2021-10-14 18:15:57 +0800

    ulrs 正则匹配

--- Athena/urls.py
+++ Athena/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.hello_world),
-    path('index', views.index),
+     path('', views.hello_world),
+     path('index', views.index),
+     re_path(r'^index/(?P<year>[0-9]{4})/$', views.index_year),
+     re_path(r'^index/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', views.index_year_month),
 ]
--- Athena/views.py
+++ Athena/views.py
@@ -11,3 +11,12 @@ def hello_world(request):
 # 模板返回
 def index(request):
     return render(request, 'athena_templates/index.html', {'welcome': 'hello'})
+
+
+# 接收url中的一部分参数
+def index_year(request, year):
+    return render(request, 'athena_templates/index.html', {'year': year})
+
+
+def index_year_month(request, year, month):
+    return render(request, 'athena_templates/index.html', {'year': year, 'month': month})
