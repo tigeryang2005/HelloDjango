e8deee8c5555c6d26f670f7cd39278049a8db287
tiger <yangtiger2005@163.com>
2021-12-07 11:07:18 +0800

    django restframework 使用ModelSerializers

--- Athena/serializers.py
+++ Athena/serializers.py
@@ -5,34 +5,9 @@ from Athena.models import Stock
 
 
 class StockSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    code = serializers.CharField(required=True, max_length=10)
-    date = serializers.DateField()
-    open = serializers.FloatField()
-    close = serializers.FloatField()
-    zhang_die = serializers.FloatField()
-    zhang_die_fu = serializers.CharField(max_length=20)
-    highest = serializers.FloatField()
-    lowest = serializers.FloatField()
-    cheng_jiao_liang = serializers.IntegerField()
-    cheng_jiao_e = serializers.FloatField()
-    huan_shou_lv = serializers.CharField(max_length=10)
-
-    def create(self, validated_data):
-        return Stock.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.code = validated_data.get('code', instance.code)
-        instance.date = validated_data.get('date', instance.date)
-        instance.open = validated_data.get('open', instance.open)
-        instance.close = validated_data.get('close', instance.close)
-        instance.zhang_die = validated_data.get('zhang_die', instance.zhang_die)
-        instance.zhang_die_fu = validated_data.get('zhang_die_fu', instance.zhang_die_fu)
-        instance.highest = validated_data.get('highest', instance.highest)
-        instance.lowest = validated_data.get('lowest', instance.lowest)
-        instance.cheng_jiao_liang = validated_data('cheng_jiao_liang', instance.cheng_jiao_liang)
-        instance.cheng_jiao_e = validated_data('cheng_jiao_e', instance.cheng_jiao_e)
-        instance.huan_shou_lv = validated_data('huan_shou_lv', instance.huan_shou_lv)
+    class Meta:
+        model = Stock
+        # fields = ('id', 'code')
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
